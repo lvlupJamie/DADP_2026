103d69e2769d00e606d07cc55568d8354ec8a37f
lvlupJamie <aeboltt@gmail.com>
2026-09-10 15:24:07 +0200

    game stuff

--- DADP_Class/Assets/Scripts/CameraController.cs
+++ DADP_Class/Assets/Scripts/CameraController.cs
@@ -5,13 +5,19 @@ using UnityEngine.InputSystem;
 public class LevelController : MonoBehaviour
 {
     [SerializeField] GameObject objectPrefab;
+    public float CameraUpSpeed = 3f; 
 
     public void OnCameraUp(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             ToggleObjects();
-            //moveSpeed = 3f;
+            //moveSpeed = CameraUpSpeed;
+        }
+        if (context.canceled)
+        {
+            ToggleObjects();
+            //moveSpeed = Original Move Speed
         }
     }
 
@@ -21,12 +27,4 @@ public class LevelController : MonoBehaviour
         objectPrefab.SetActive(!currentState);
     }
 
-    /*
-     List of things to do (in order of importance)
-        (DONE)  1. Make it so that instead of a single object, and entire layer is effected
-        (DONE)  2. Add UI indication that camera is up
-                3. Add a slight delay to the action
-        (DONE)  4. Make player slower while camera is up
-                5. Create a UI for the camera coming up and down
-    */
 }
